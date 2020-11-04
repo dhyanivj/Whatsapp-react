744813b61cc959a74bb7f6ab90f88f55c7b4f555
dhyanivj <vijay97dhyani@gmail.com>
2020-11-04 07:12:59 -0500

    getting started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState} from "react";
 import "./style.css";
 
 export default function App() {
+  const [todos, setTodos] = useState(['take a mango','go to the school']);
+  
   return (
-    <div>
-   can i start it now 
+    <div className="App">
+    <h1>cleaver progammer</h1>
+    <input/>
+    <button>Add todo </button>
+<ul>
+<li>
+</li>
+</ul>
+
     </div>
   );
 }
