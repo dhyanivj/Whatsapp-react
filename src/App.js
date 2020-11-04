b81375a3a1cca6a9447468eecdb03601d4de890e
dhyanivj <vijay97dhyani@gmail.com>
2020-11-04 08:17:31 -0500

    btn working

--- src/App.js
+++ src/App.js
@@ -11,16 +11,23 @@ export default function App() {
 
   const addTodo = event => {
     event.preventDefault(); //stop refeshing page
-    setTodos([...todo, input]); //add todo tat last 
-    setInput('');  //blank input field
+    setTodos([...todo, input]); //add todo tat last
+    setInput(""); //blank input field
   };
 
   return (
     <div className="App">
       <h1>Todo App</h1>
       <form>
-      <input value={input} onChange={event => setInput(event.target.value)} />
-      <button type="submit" onClick={addTodo}>Add todo </button>
+        <input
+          value={input}
+          onChange={event => setInput(event.target.value)}
+          className="input"
+        />
+
+        <button type="submit" onClick={addTodo} className="btn">
+          Add todo{" "}
+        </button>
       </form>
       <ul>
         {todo.map(todo => (
